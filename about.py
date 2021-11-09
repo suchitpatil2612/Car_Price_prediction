fae99bf7e1dce38a8bf2f62ffcfe4760cebf4d76
suchitpatil2612 <s77206291+suchitpatil2612@users.noreply.github.com>
2021-11-09 23:43:28 +0530

    first commit

---
+++ about.py
@@ -0,0 +1,14 @@
+"""This create About me page"""
+
+# Import necessary module
+import streamlit as st
+
+def app():
+    st.balloons()
+    st.title('Contact Us')
+    st.markdown('''### Name:
+    Suchit Patil ''')
+    st.markdown('''### Email:
+    suchitpatil2612@gmail.com''')
+    st.markdown('''### GitHub: [SuchitPatil](https://github.com/suchitpatil2612)''')
+    st.markdown('''### Lindin: [SuchitPatil](https://www.linkedin.com/in/suchit-p/)''')
